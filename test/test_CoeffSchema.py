5f240bc8c19127bab0501a540a2117addd54f56f
James Holley <james.j.holley@nasa.gov>
2016-01-27 15:21:04 -0600

    added output for files that do not meet the coeff schema pattern

--- test/test_CoeffSchema.py
+++ test/test_CoeffSchema.py
@@ -4,23 +4,33 @@ import unittest
 import glob, os
 import CoeffSchemaDefinitions
 from lxml import etree as xmlParser
+import lxml
+import logging
 
 class coeffFileTests(unittest.TestCase):
 
     def setUp(self):
         self.testDirectory = os.path.dirname(os.path.abspath(__file__))
+        self.log = logging.getLogger("Coeff Test Logger")
 
     def tearDown(self):
         pass
 
-    def testCoeffValidSchema(self):
+    def testActuatorCoeffsValidSchema(self):
         schema_root = xmlParser.XML(CoeffSchemaDefinitions.std_coeff)
         schema = xmlParser.XMLSchema(schema_root)
         parser = xmlParser.XMLParser(schema = schema)
         coeffFileDirectory = self.testDirectory + '/../instance/coefficients/actuators'
         os.chdir(coeffFileDirectory)
+        correctFiles = []
+        incorrectFiles = []
         for coeffFile in glob.glob("*.xml"):
-            root = xmlParser.parse(coeffFile, parser)
-
+            try:
+                root = xmlParser.parse(coeffFile, parser)
+                correctFiles.append(coeffFile)
+            except xmlParser.XMLSyntaxError:
+                self.log.error(coeffFile + " has coeffs that are not in the schema")
+                incorrectFiles.append(coeffFile)
+        assert len(incorrectFiles) == 0
 
 
